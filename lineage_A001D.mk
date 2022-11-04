bcfea1d19d4650b55c86acc3d8666437fd566c88
criperm19 <96837536+CreeperAmigo19@users.noreply.github.com>
2022-11-03 22:09:45 -0300

    Update lineage_A001D.mk

--- lineage_A001D.mk
+++ lineage_A001D.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from A001D device
 $(call inherit-product, device/asus/A001D/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+# Inherit some common CarbonRom stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A001D
