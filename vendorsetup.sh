957e307e718fe36a24b354dc89b21652ac436e85
redznn <h908659@gmail.com>
2023-04-30 19:41:40 -0300

    A001D: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+echo "Cloning Max Shot Devixe Tree and stuff"
+git clone https://github.com/GuilianoS/kernel_asus_A001D.git kernel/asus/A001D
+git clone https://github.com/GuilianoS/vendor_asus_A001D.git vendor/asus/A001D
