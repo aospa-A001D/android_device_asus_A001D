4d1cb1a83bd5cfa4970c0459d5c82b1d7bc7c4d7
criperm19 <96837536+criperm19@users.noreply.github.com>
2023-05-09 19:56:47 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 echo "Cloning Max Shot Devixe Tree and stuff"
-git clone https://github.com/GuilianoS/kernel_asus_A001D.git kernel/asus/A001D
-git clone https://github.com/GuilianoS/vendor_asus_A001D.git vendor/asus/A001D
+git clone https://github.com/GuilianoS/kernel_asus_A001D.git -b 11 kernel/asus/A001D
+git clone https://github.com/GuilianoS/vendor_asus_A001D.git -b lineage-18.1 vendor/asus/A001D
